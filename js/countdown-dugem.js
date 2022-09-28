2ca8ca56f5e79e898772ff27c0d37b503be794ad
hatt-hust <hatt.hust@gmail.com>
2022-09-28 17:51:57 +0700

    changed font

--- js/countdown-dugem.js
+++ js/countdown-dugem.js
@@ -7,7 +7,7 @@
          hour = minute * 60,
          day = hour * 24;
 
-         let birthday = "Dec 31, 2021 00:00:00",
+         const birthday = "Dec 31, 2022 00:00:00",
          countDown = new Date(birthday).getTime(),
          x = setInterval(function() {
 
